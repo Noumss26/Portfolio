16b788993117d76423fbd5bf67e2e506da00611b
Noumss26 <nomenajeiel@gmail.com>
2025-07-26 19:44:18 +0300

    premier commit

--- src/app/ui/SplineClient.tsx
+++ src/app/ui/SplineClient.tsx
@@ -1,21 +1,38 @@
 'use client';
 
-import dynamic from 'next/dynamic';
-
-const Spline = dynamic(() => import('@splinetool/react-spline'), {
-  ssr: false,
-});
+import { useEffect, useState } from 'react';
 
 export default function SplineClient() {
+  const [Spline, setSpline] = useState<any>(null);
+
+  useEffect(() => {
+    const loadSpline = async () => {
+      try {
+        const splineModule = await import('@splinetool/react-spline');
+        setSpline(() => splineModule.default || splineModule);
+      } catch (error) {
+        console.error('Error loading Spline:', error);
+      }
+    };
+
+    loadSpline();
+  }, []);
+
+  if (!Spline) {
+    return (
+      <div className="w-[65vh] h-[60vh] flex items-center justify-center">
+        <div>Loading 3D scene...</div>
+      </div>
+    );
+  }
+
   return (
     <div className="w-[65vh] h-[60vh]">
       <Spline
         scene="https://prod.spline.design/qZHLZ0VE7hzeEUFc/scene.splinecode"
         onLoad={() => console.log("Spline loaded")}
-        onError={(e) => console.error("Spline error:", e)}
+        onError={(e: any) => console.error("Spline error:", e)}
       />
-
-      {/* <Spline scene="https://prod.spline.design/qZHLZ0VE7hzeEUFc/scene.splinecode" /> */}
     </div>
   );
 }
